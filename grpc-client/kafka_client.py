c4c11f599f6d493760cfe9ad65ac970b22b06ca7
Parth Sinha <sinhaparth555@gmail.com>
2024-07-27 19:58:31 +0100

    left over code added

--- grpc-client/kafka_client.py
+++ grpc-client/kafka_client.py
@@ -1,17 +1,17 @@
 import grpc
-import kafka_service_pb2
-import kafka_service_pb2_grpc
+import kafka_client_pb2
+import kafka_client_pb2_grpc
 
 def run():
     channel = grpc.insecure_channel('grpc_server:50051')
-    stub = kafka_service_pb2_grpc.KafkaServiceStub(channel)
+    stub = kafka_client_pb2_grpc.KafkaServiceStub(channel)
 
     # Produce a message
-    response = stub.ProduceMessage(kafka_service_pb2.ProduceRequest(topic='test', message='Hello, Kafka!'))
+    response = stub.ProduceMessage(kafka_client_pb2.ProduceRequest(topic='test', message='Hello, Kafka!'))
     print(f"ProduceMessage success: {response.success}")
 
     # Consume messages
-    response_iterator = stub.ConsumeMessages(kafka_service_pb2.ConsumeRequest(topic='test'))
+    response_iterator = stub.ConsumeMessages(kafka_client_pb2.ConsumeRequest(topic='test'))
     for response in response_iterator:
         print(f"Consumed message: {response.message}")
 
